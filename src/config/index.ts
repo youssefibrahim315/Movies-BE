8493cb94515881d31a7000ee5b86ede32176c3d1
youssefibrahim315 <youssefibrahim315@gmail.com>
2022-09-28 08:11:09 +0200

    configFile

---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+export const configuration = {
+  server: {
+    port: 3000,
+  },
+  dataBase: {
+    url: "mongodb+srv://movies:movies@cluster0.w75zoik.mongodb.net/?retryWrites=true&w=majority",
+  },
+  middleware: {},
+  services: {},
+};
